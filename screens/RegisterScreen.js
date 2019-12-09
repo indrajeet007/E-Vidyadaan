518b8a87a6ea971b6cf2e5164e892c28bb069ab1
indrajeet007 <indrajeet.0072@gmail.com>
2019-12-10 01:23:33 +0530

    register screen done along with function calls

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -5,7 +5,6 @@ import { registerUser } from '../services/User.service'
 
 export class RegisterScreen extends Component {
     state = {
-        user: {},
         email: '',
         password: ''
     }
@@ -36,26 +35,21 @@ export class RegisterScreen extends Component {
     }
 
     _redirectToLogin = () => {
-        let userData = {
-            username: 'abc',
+        const { navigation } = this.props
+        const userData = {
             email: this.state.email,
-            password: this.state.password
+            password: this.state.password,
+            username: this.state.email.replace(/@[^@]+$/, '')
         }
 
-        this.setState({
-            user: userData
-        }, () => console.log('userData ===>', this.state.user))
-
-        registerUser(this.state.user).then(res => {
-            console.log("data coming from backend is ===> ",res.data)
+        registerUser(userData).then(res => {
+            console.log("data coming from backend is ===> ", res.data)
         })
         .catch(err => {
             console.log("error ==>", err)
         })
 
-        // const { navigation } = this.props
-        // // console.log('redirect to login clicked!', navigation)
-        // navigation.navigate("Login")
+        navigation.navigate("Login")
     }
     
     render() {
@@ -63,8 +57,19 @@ export class RegisterScreen extends Component {
             <View style={{flex: 1, flexDirection: 'column', justifyContent:'space-around', alignItems:'center', backgroundColor:'teal', paddingTop: 200}}>
                 <View style={{ flex: 1, width: 300, height: 50, paddingBottom: 50 }}>
                     <Text style={{textAlign: 'center', fontSize: 25, paddingBottom: 20}}>Register</Text>
-                    <Input onChangeText={this.handleEmailChange} value={this.state.email} label='Enter Email' placeholder='email@address.com'/>
-                    <Input onChangeText={this.handlePasswordChange} value={this.state.password} label='Enter Password' placeholder='Password'/>
+                    <Input 
+                        onChangeText={this.handleEmailChange} 
+                        value={this.state.email} 
+                        label='Enter Email' 
+                        placeholder='email@address.com' 
+                        autoCapitalize='none'/>
+                    <Input 
+                        onChangeText={this.handlePasswordChange} 
+                        value={this.state.password} 
+                        label='Enter Password' 
+                        placeholder='Password'
+                        autoCapitalize='none'
+                        secureTextEntry={true}/>
                 </View>
                 <View style={{ flex: 2, width: 150, height: 50 }}>
                     <Button onPress={() => this._redirectToLogin()} title="Register"/>
